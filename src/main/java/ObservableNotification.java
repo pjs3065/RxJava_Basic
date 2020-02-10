3a4f2d6cec85176eb662c2bc3982b24f95ec8830
pjs3065 <tmakdlfwotjd@naver.com>
2020-02-11 01:04:08 +0900

    Disposable 인터페이스 함수 사용하기

---
+++ src/main/java/ObservableNotification.java
@@ -0,0 +1,20 @@
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+
+public class ObservableNotification {
+     Observable<String> source = Observable.just("RED", "GREEN", "YELLOW");
+     Disposable d = source.subscribe(
+            v -> System.out.println("onNext() : value : " + v),
+            err -> System.out.println("onError() : err : " + err.getMessage()),
+            () -> System.out.println("onComplete()")
+    );
+     
+    public static void main(String[] args) {
+        ObservableNotification on = new ObservableNotification();
+        System.out.println("isDisposed() : " + on.d.isDisposed());
+    }
+}
+
+
+
+
