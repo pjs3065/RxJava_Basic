d7a6f95d465c1d46d759927a00a83a52c961bb5b
pjs3065 <tmakdlfwotjd@naver.com>
2020-02-11 20:34:32 +0900

    메소드 레퍼런스와 람다식과 익명객체 사용 예제 추가

--- src/main/java/ObservableCreateExample.java
+++ src/main/java/ObservableCreateExample.java
@@ -1,14 +1,27 @@
 import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 
 public class ObservableCreateExample {
     public static void main(String[] args) {
         Observable<Integer> source = Observable.create(
-                emitter ->{
+                emitter -> {
                     emitter.onNext(100);
                     emitter.onNext(200);
                     emitter.onNext(300);
                 });
 
-        source.subscribe(System.out::println);
+        //메소드 레퍼런스로 축약 사용
+//        source.subscribe(System.out::println);
+
+        //람다로 사용
+//        source.subscribe(data -> System.out.println(data));
+
+        //익명객체로 사용
+        source.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer data) throws Exception {
+                System.out.println(data);
+            }
+        });
     }
 }
