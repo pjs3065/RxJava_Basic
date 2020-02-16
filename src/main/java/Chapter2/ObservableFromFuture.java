b602a0725f8784ef05f63c566ffc8856a8a744c0
pjs3065 <tmakdlfwotjd@naver.com>
2020-02-16 22:59:19 +0900

    fromFuture()함수 코드

---
+++ src/main/java/Chapter2/ObservableFromFuture.java
@@ -0,0 +1,26 @@
+package Chapter2;
+
+import io.reactivex.Observable;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ObservableFromFuture {
+
+    /**
+     * fromFuture() 함수를 이용하여 Observable 생성하기
+     */
+    private void basic(){
+        Future<String> future = Executors.newSingleThreadExecutor().submit(() -> {
+            Thread.sleep(1000);
+            return "Hello Future";
+        });
+        Observable<String> source = Observable.fromFuture(future);
+        source.subscribe(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        ObservableFromFuture obj = new ObservableFromFuture();
+        obj.basic();
+    }
+}
