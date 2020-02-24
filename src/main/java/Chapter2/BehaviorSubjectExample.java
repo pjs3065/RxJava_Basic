e5455a77cece035c9f4cc61d9e69ddc4c8ddb9ab
pjs3065 <tmakdlfwotjd@naver.com>
2020-02-25 00:11:18 +0900

    BehaviorSubject 클래스 예제

---
+++ src/main/java/Chapter2/BehaviorSubjectExample.java
@@ -0,0 +1,21 @@
+package Chapter2;
+
+import io.reactivex.subjects.BehaviorSubject;
+
+public class BehaviorSubjectExample {
+
+    public void marbleDiagram(){
+        BehaviorSubject<String> subject = BehaviorSubject.createDefault("pink");
+        subject.subscribe(data -> System.out.println("Subscriber #1 => " + data ));
+        subject.onNext("red");
+        subject.onNext("green");
+        subject.subscribe(data -> System.out.println("Subscriber #2 => " + data ));
+        subject.onNext("blue");
+        subject.onComplete();
+    }
+
+    public static void main(String[] args) {
+        BehaviorSubjectExample obj = new BehaviorSubjectExample();
+        obj.marbleDiagram();
+    }
+}
